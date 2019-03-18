1b93b53af5fe91d02cdf6a96a01210fe15ba47e8
prince-curie <prince.chibuike@gmail.com>
2019-03-18 20:12:25 +0100

    chore(model): create meals model > - meals.models.js > > chore #164708484

---
+++ api/models/meals.model.js
@@ -0,0 +1,11 @@
+class meals {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.price = null;
+    this.size = null;
+    this.status = null;
+  }
+}
+
+export default meals;
