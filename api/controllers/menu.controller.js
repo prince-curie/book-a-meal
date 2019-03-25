4e9a944cf25604e488fb35e02841f536ac395c07
prince-curie <prince.chibuike@gmail.com>
2019-03-25 14:59:43 +0100

    feat(caterer): caterer needs a get menu endpoint - this ccommit consist of the middleware for such endpoint
    feat #164026371

--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -1,6 +1,6 @@
 import menuService from '../services/menu.service';
 
-const { addMenuDB } = menuService;
+const { addMenuDB, fetchAllMenuDB } = menuService;
 const controllerReturns = (res, statusNo, dataFn) => res.json({
   status: statusNo,
   data: dataFn,
@@ -9,6 +9,9 @@ const controllerReturns = (res, statusNo, dataFn) => res.json({
 const menuController = {
   addMenu(req, res) {
     return controllerReturns(res, 201, [addMenuDB(req)]);
+  },
+  fetchAllMenu(req, res) {
+    return controllerReturns(res, 200, fetchAllMenuDB());
   },
 };
 
