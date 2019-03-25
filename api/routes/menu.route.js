5e899acf00d1d30f3d1dd6e482ef567341d6cc13
prince-curie <prince.chibuike@gmail.com>
2019-03-25 14:57:31 +0100

    feat(routes): create routes for the get menu endpoint - menu.route.js  feat #164026371

--- api/routes/menu.route.js
+++ api/routes/menu.route.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import menuController from '../controllers/menu.controller';
 import menuAuth from '../auth/menu.auth';
 
-const { addMenu } = menuController;
+const { addMenu, fetchAllMenu } = menuController;
 const { authAddMenu } = menuAuth;
 const router = Router();
 
 router.post('/menu', authAddMenu, addMenu);
+
+router.get('/menu', fetchAllMenu);
 
 export default router;
