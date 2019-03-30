b32502012f1601a85bc8c453773855d5f205c428
prince-curie <prince.chibuike@gmail.com>
2019-03-30 08:15:59 +0100

    create database connection - test database connection

--- api/database/database.js
+++ api/database/database.js
@@ -1,32 +1,17 @@
-export default {
-  meals: [
-    {
-      id: 1,
-      name: 'rice and beans',
-      price: 500,
-      size: 'medium',
-      status: 'available',
-    },
-    {
-      id: 2,
-      name: 'bread and beans',
-      price: 200,
-      size: 'small',
-      status: 'available',
-    },
-    {
-      id: 3,
-      name: 'eba and egusi',
-      price: 400,
-      size: 'medium',
-      status: 'unavailable',
-    },
-    {
-      id: 4,
-      name: 'yam and egg',
-      price: 300,
-      size: 'medium',
-      status: 'unavailable',
-    },
-  ],
-};
+import Sequelize from 'sequelize';
+
+const database = new Sequelize('bookmeal', 'Chibuike', 'pass', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+database
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+export default database;
