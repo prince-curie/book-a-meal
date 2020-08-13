5017e09904a17f1cd52c943fc649a9bd24ef939c
Prince-Curie <chibuike@crenettechlabs.com>
2020-08-13 19:04:04 +0100

    creates database config

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWWORD,
+    database: process.env.TEST_DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    operatorsAliases: false,
+  },
+};
